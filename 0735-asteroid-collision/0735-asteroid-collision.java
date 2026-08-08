3dbfb52439f872846a6b77fe8c709e438b3ffc76
VTU28542 <vtu28542@veltech.edu.in>
2026-08-08 15:08:40 +0530

    Time: 5 ms (68.67%), Space: 46.9 MB (59.43%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int asteroid : asteroids) {
+
+            boolean destroyed = false;
+
+            // Collision happens only when:
+            // stack top is moving right (+)
+            // current asteroid is moving left (-)
+            while (!stack.isEmpty() &&
+                   stack.peek() > 0 &&
+                   asteroid < 0) {
+
+                if (stack.peek() < -asteroid) {
+                    // Top asteroid explodes
+                    stack.pop();
+                }
+                else if (stack.peek() == -asteroid) {
+                    // Both explode
+                    stack.pop();
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    // Current asteroid explodes
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                stack.push(asteroid);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+
+        for (int i = 0; i < stack.size(); i++) {
+            result[i] = stack.get(i);
+        }
+
+        return result;
+    }
+}
